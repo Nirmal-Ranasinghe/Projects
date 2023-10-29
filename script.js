b437a82869a7cb1e09f04d0efff671e03d46e564
[Nirmal Ranasinghe] <[nirmalranasinha@gmail.com]>
2023-10-29 15:47:20 +0530

    first commet upload the branch

---
+++ script.js
@@ -0,0 +1,24 @@
+/*console.log(`Helo  wolrd`);
+console.log(`I like coding`);
+
+window.alert(`This is an alert`);
+window.alert(`I like coding`);*/
+
+
+document.getElementById("id1").textContent = `hello`;
+
+
+
+
+
+
+
+
+
+
+
+
+//This is a comment
+/*
+This is a commetn
+ do you know that you idiot*/
