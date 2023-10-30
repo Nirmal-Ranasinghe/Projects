b2728147a8e87a60968debab6ee68d738fa84367
[Nirmal Ranasinghe] <[nirmalranasinha@gmail.com]>
2023-10-30 23:09:47 +0530

    new tut

--- script.js
+++ script.js
@@ -2,18 +2,27 @@
 console.log(`I like coding`);
 
 window.alert(`This is an alert`);
-window.alert(`I like coding`);*/
-
+window.alert(`I like coding`);
 
 document.getElementById("id1").textContent = `hello`;
+document.getElementById("id2").textContent = `I like coding`;
+
+let age = 23;
+let cost =  10.99
+let gpa = 3.8
+console.log(`your are ${age} years old`);
+console.log(typeof gpa);
+console.log(`yohave to pay $${cost}`);
+console.log(`your gpa is ${gpa}`);
 
 
-
-
-
-
-
-
+let firstName = "Bro";
+let food = "Pizza"
+console.log(typeof firstName);
+console.log(firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`my favourite food is ${food}`);
+*/
 
 
 
