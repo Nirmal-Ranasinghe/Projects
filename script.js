b782f4bfca7e193bf743f48c9e4a52600ead137a
[Nirmal Ranasinghe] <[nirmalranasinha@gmail.com]>
2023-10-31 19:23:35 +0530

    data-types

--- script.js
+++ script.js
@@ -15,19 +15,99 @@ console.log(typeof gpa);
 console.log(`yohave to pay $${cost}`);
 console.log(`your gpa is ${gpa}`);
 
-
 let firstName = "Bro";
 let food = "Pizza"
 console.log(typeof firstName);
 console.log(firstName);
 console.log(`Your name is ${firstName}`);
 console.log(`my favourite food is ${food}`);
+
+let online = false;
+let forsale = true;
+console.log(typeof online);
+console.log(`I am online; ${online}`);
+console.log(`Is this car for sale: ${forsale}`);
+
+let fullName = "Nirmal";
+let age = 23;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`
+document.getElementById("p2").textContent = `I am ${age} years old`
+document.getElementById("p3").textContent = `Are you a student?: ${student}`
+
+let student = 30;
+
+//student = student + 1 
+//student = student ** 2
+//student = student % 2
+//let exstudent = student + 1
+
+//student += 1;
+//student **= 2;
+
+//student++;
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
 */
 
+/*let username;
+
+username = window.prompt("What's your user name?");
+console.log(username);
+let username;
+
+let username;
+
+document.getElementById("btn").onclick = function(){
+    username = document.getElementById("mytext").value;
+    console.log(username);
+    document.getElementById("H1").textContent = `Hello ${username}`
+}
+
+let age =  window.prompt("How old are you");
+
+//age = Number(age);
+age +=1;
+console.log(age, typeof age) 
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z); */
 
 
 
-//This is a comment
-/*
-This is a commetn
- do you know that you idiot*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
